4caf0de1f8febc26e7603847b180557867aeda46
chrisdborges <chrisdborges@gmail.com>
2020-08-10 19:10:43 -0300

    login.html y login.js
    no pude hacer que al ingresar cualquier dato te direccione a index

--- js/login.js
+++ js/login.js
@@ -1,6 +1,47 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var usersArray = [];
 
-});
+function validateUser(array, userIn, passwordIn) {
+    for (let i = 0; i < array.length; i++) {
+        let usuario = array[i];
+        if (usuario.email == userIn && usuario.password == passwordIn) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    document.getElementById("submitBtn").addEventListener("click", function (e) {
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPassword");
+        let camposCompletos = true;
+
+        if (inputEmail.value === '') {
+            inputEmail.classList.add("invalid");
+            camposCompletos = false;
+        }
+
+        if (inputPassword.value === '') {
+            inputPassword.classList.add("invalid");
+            camposCompletos = false;
+        }
+
+        if (camposCompletos) {
+            getJSONData().then(function (resultObj) {
+                if (resultObj.status === "ok") {
+                    usersArray = resultObj.data;
+
+                    if (validateUser(usersArray, inputEmail.value, inputPassword.value)) {
+                        window.location = 'index.html';
+                    } else {
+                        alert("Usuario o contraseña incorrectos!");
+                    }
+                }
+            });
+        } else {
+            alert("Debes ingresar los datos!");
+        }
+    })
+})
