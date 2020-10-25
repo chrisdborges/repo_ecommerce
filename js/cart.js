3162fc565a770698489b029f086afa281b9a4ab3
chrisdborges <chrisdborges@gmail.com>
2020-10-25 20:53:01 -0300

    Entrega 6
    Modal con tarjetas y confirmación de pago.

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,5 @@
 var currency = "USD";
 
-
 function showCart() {
 
     $('#cart-table-list').empty();
@@ -148,3 +147,36 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 
 });
+
+$(function () {
+    var $form = $("#payment-form");
+
+    $form.on('submit', function (e) {
+        if (!$form.data('cc-on-file')) {
+            e.preventDefault();
+            Stripe.setPublishableKey($form.data('stripe-publishable-key'));
+            Stripe.createToken({
+                number: $('.card-number').val(),
+                cvc: $('.card-cvc').val(),
+                exp_month: $('.card-expiry-month').val(),
+                exp_year: $('.card-expiry-year').val()
+            }, stripeResponseHandler);
+        }
+    });
+
+    function stripeResponseHandler(status, response) {
+        if (response.error) {
+            $('.error')
+                .removeClass('hide')
+                .find('.alert')
+                .text(response.error.message);
+        } else {
+            // token contains id, last4, and card type
+            var token = response['id'];
+            // insert the token into the form so it gets submitted to the server
+            $form.find('input[type=text]').empty();
+            $form.append("<input type='hidden' name='reservation[stripe_token]' value='" + token + "'/>");
+            $form.get(0).submit();
+        }
+    }
+})
